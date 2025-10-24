e99bce87847773e1e742f4f7820574562a00a32b
Sergio Lopez Ruiz <sergi.lopez.ruiz@gmail.com>
2025-10-24 10:05:50 -0400

    Added search by price function to reach 95% coverage

--- tests/test_models.py
+++ tests/test_models.py
@@ -298,3 +298,22 @@ class TestProductModel(unittest.TestCase):
                     "category": product.category.name  # convert enum to string
                 }
             )
+
+    def test_find_products_by_price(self):
+        """It should find a product by price"""
+        products = ProductFactory.create_batch(10)
+
+        for product in products:
+            product.create()
+
+        search_price = products[0].price
+
+        count = len([product for product in products if product.price == search_price])
+        found = Product.find_by_price(search_price)
+
+        # Same amount found manually and with the function
+        self.assertEqual(found.count(), count)
+
+        # All founds have the same name
+        for product in found:
+            self.assertEqual(product.price, search_price)
